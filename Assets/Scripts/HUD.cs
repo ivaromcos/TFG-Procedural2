222c98f553c6ce053bfc55a587f8b4d787711128
Ivan Romero Costa <ivaromcos@alum.us.es>
2023-11-07 20:37:14 +0100

    HUD y perdida de vida
    Implementacion del HUD y del sistema de vida asi como la perdida de vidas por colision

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+
+    public GameObject[] vidas;
+    public void DesactivarVida(int indice) {
+        vidas[indice].SetActive(false);
+    }
+    public void ActivarVida(int indice)
+    {
+        vidas[indice].SetActive(true);
+    }
+}
